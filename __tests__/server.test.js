f14fe5fdc7110bd0534e521ad017353e20d79945
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2022-12-30 15:20:56 +0000

    fixd duplication in server test file

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -14,19 +14,3 @@ test('goodbye endpoint', async () => {
   const response = await request.get('/goodbye');
   expect(response.text).toBe('Nailed it!');
 });
-const server = require('./server');
-const supertest = require('supertest');
-
-// const { default: test } = require('node:test');
-
-const request = supertest(server);
-
-test('hello endpoint', async () => {
-  const response = await request.get('/hello');
-  expect(response.text).toBe('Hello!');
-});
-
-test('goodbye endpoint', async () => {
-  const response = await request.get('/goodbye');
-  expect(response.text).toBe('Nailed it!');
-});
