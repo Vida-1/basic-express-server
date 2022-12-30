da2a30c2c234aa3e963f003e6b230328c6a55eb0
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2022-12-30 16:26:23 +0000

    incomplete, late, initial canvas submission in case current interruption lasts longer than expected

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,5 +1,6 @@
 const server = require('../src/server');
 const supertest = require('supertest');
+const { get } = require('../src/server');
 
 // const { default: test } = require('node:test');
 
@@ -13,4 +14,23 @@ test('hello endpoint', async () => {
 test('goodbye endpoint', async () => {
   const response = await request.get('/goodbye');
   expect(response.text).toBe('Nailed it!');
+});
+
+describe('Person Route', () => {
+  // Person Route
+  // Method: GET
+  // Path: /person
+  // Expectes a query string from the user with the "name" property
+
+  test('When query string present, output JSON to the client with this shape: {name: "name provided" }', async () => {
+    const response = await request.get('/person?name=Picard');
+    expect(response.statusCode).toBe(200);                    // toBe is deep equality triple equals
+    expect(response.body).toEqual({ name: 'Picard' });   // toEqual looks at properties 
+ 
+  });
+
+  test.todo("Without a name in the query string, force a 500 error"), async() => {
+    const response = await request.get('/person');
+    expect(response.statusCode).toBe(500);
+  };
 });
