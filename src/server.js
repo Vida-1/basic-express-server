edefc5b80a8763eb911dcf3f36c7fac9d19dcce5
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2023-01-02 02:38:50 +0000

    did a prettier --write src to hopefully resolve test failure on gitHub

--- src/server.js
+++ src/server.js
@@ -1,44 +1,46 @@
 //Attribution: this code is 99% from class demo for lab 1
-const express = require('express'); 
+const express = require('express');
 const server = express();
 
-server.use(logger); 
+server.use(logger);
 server.get('/hello', (req, res) => res.send('Hello!'));
 server.use((req, res, next) => {
   if (req.method === 'GET' && req.path.startsWith('/hello')) {
-  res.send("Hi there!");
-} else {
-  next();
-}  
+    res.send('Hi there!');
+  } else {
+    next();
+  }
 });
 
 //Note: Express middlewares operate in the order in which they have been added (called) to the server (not necessarily the order in which they were defined).
 
 server.get('/hello', (req, res) => res.send('Hello!'));
-server.get('/hello/:name', (req, res) =>
-  res / send(`Hello, ${req.params.name}!`)
+server.get(
+  '/hello/:name',
+  (req, res) => res / send(`Hello, ${req.params.name}!`)
 );
 
 server.get('/goodbye', (req, res) => res.send('Nailed it!'));
 
 // by convention this middleware is called a validator, it will check something on the request. If it's there great if it's not it will throw an error.
 const nameValidator = (req, _, next) => {
-  if (req.query?.name) {  //"optional chaining"
+  if (req.query?.name) {
+    //"optional chaining"
     req.name = req.query.name;
     next();
   } else {
-    next("Failed validation: No name in query!");
+    next('Failed validation: No name in query!');
   }
-}
+};
 
 server.use(nameValidator);
 
 server.get('/person', (req, res) => {
-//  if(req.query.name){
-    res.status(200).send({name: req.query.name});
-//  } else {
-//    res.status(500).send();
-//  }
+  //  if(req.query.name){
+  res.status(200).send({ name: req.query.name });
+  //  } else {
+  //    res.status(500).send();
+  //  }
 });
 
 server.get('/throw_error', () => {
@@ -49,10 +51,12 @@ server.get('/pass_error', (req, res, next) => {
   next('Something bad');
 });
 
- server.use('*', (_, res) => res.status(404).send("No error handler found, sorry Charlie!"));
+server.use('*', (_, res) =>
+  res.status(404).send('No error handler found, sorry Charlie!')
+);
 
 server.use((err, req, res, next) => {
-  res.status(500).send({message: "there was a problem!", err})
+  res.status(500).send({ message: 'there was a problem!', err });
 });
 
 module.exports = {
